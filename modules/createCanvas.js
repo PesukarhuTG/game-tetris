91bcd3567f7b6e0ea78feb3644659d580a5cabfd
Tatiana <ganzhinovat@yandex.ru>
2022-04-21 12:14:03 +0300

    refactor: add View module

--- modules/createCanvas.js
+++ modules/createCanvas.js
@@ -1,54 +1,14 @@
 import { Game } from './game.js';
+import { View } from './view.js';
 
 const game = new Game();
+const view = new View(document.querySelector('.container'));
 
+export const SIZE_BLOCK = 30;
 export const COLUMNS = 10;
 export const ROWS = 20;
 
 const createCanvas = () => {
-  const SIZE_BLOCK = 30;
-  const container = document.querySelector('.container');
-
-  const colors = {
-    J: 'DarkCyan',
-    I: 'MediumVioletRed',
-    O: 'Cyan',
-    L: 'Green',
-    2: 'Salmon',
-    T: 'BlueViolet',
-    S: 'Chocolate'
-  };
-
-  const canvas = document.createElement('canvas');
-  canvas.classList.add('game-area');
-  container.append(canvas);
-
-  canvas.width = SIZE_BLOCK * COLUMNS;
-  canvas.height = SIZE_BLOCK * ROWS;
-
-  //need to create the context for canvas
-  const context = canvas.getContext('2d');
-
-  const showArea = area => {
-    //Step2:clear figure's step
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let y = 0; y < area.length; y++) {
-      const line = area[y];
-
-      for (let x = 0; x < line.length; x++) {
-        const block = line[x];
-
-        if (block !== 'o') {
-          context.fillStyle = colors[block]; //background
-          context.strokeStyle = 'white'; //border
-          context.fillRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK); //background
-          context.strokeRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK); //border
-        }
-      }
-    }
-  };
-
   //Step1: move figure
   window.addEventListener('keydown', e => {
     const key = e.code;
@@ -56,24 +16,25 @@ const createCanvas = () => {
     switch (key) {
       case 'ArrowLeft':
         game.moveLeft();
-        showArea(game.viewArea);
+        view.querySelectorshowArea(game.viewArea);
         break;
       case 'ArrowRight':
         game.moveRight();
-        showArea(game.viewArea);
+        view.querySelectorshowArea(game.viewArea);
         break;
       case 'ArrowDown':
         game.moveDown();
-        showArea(game.viewArea);
+        view.querySelectorshowArea(game.viewArea);
         break;
       case 'ArrowUp':
         game.rotateTetromino();
-        showArea(game.viewArea);
+        view.querySelectorshowArea(game.viewArea);
         break;
     }
   });
 
-  showArea(game.viewArea);
+  view.init();
+  view.showArea(game.viewArea);
 };
 
 export default createCanvas;
---
+++ modules/view.js
@@ -0,0 +1,51 @@
+import { SIZE_BLOCK, COLUMNS, ROWS } from './createCanvas.js';
+
+export class View {
+  constructor(container) {
+    this.container = container;
+  }
+
+  colors = {
+    J: 'DarkCyan',
+    I: 'MediumVioletRed',
+    O: 'Cyan',
+    L: 'Green',
+    2: 'Salmon',
+    T: 'BlueViolet',
+    S: 'Chocolate'
+  };
+
+  canvas = document.createElement('canvas');
+
+  init() {
+    this.canvas.classList.add('game-area');
+    this.container.append(this.canvas);
+
+    this.canvas.width = SIZE_BLOCK * COLUMNS;
+    this.canvas.height = SIZE_BLOCK * ROWS;
+  }
+
+
+  //need to create the context for canvas
+  context = this.canvas.getContext('2d');
+
+  showArea(area) {
+    //Step2:clear figure's step
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    for (let y = 0; y < area.length; y++) {
+      const line = area[y];
+
+      for (let x = 0; x < line.length; x++) {
+        const block = line[x];
+
+        if (block !== 'o') {
+          this.context.fillStyle = this.colors[block]; //background
+          this.context.strokeStyle = 'white'; //border
+          this.context.fillRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK); //background
+          this.context.strokeRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK); //border
+        }
+      }
+    }
+  };
+}
